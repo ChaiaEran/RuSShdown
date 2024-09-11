36d10a1656e7ac539e38f330201fb080ea51a916
Ariel Hoult <ariel.hoult@gmail.com>
2024-09-11 11:39:55 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RuSShdown
+## A new RSS feed generator for non-coders.
+
+RuSShdown lets you make an RSS feed without touching a single line of code, with posts that support Markdown, like you'd make on cohost!
+
+RuSShdown works in your browser, uses absolutely no cookies, and runs 100% client-side. You can download the source code and pore through it yourself, if you'd like.
+
+Made by:   
+![Chaia Eran: Cute as Hell, Queer AF](https://chaiaeran.neocities.org/imgs/chaia.png)
---
+++ russhdown.js
@@ -0,0 +1,82 @@
+var converter = new showdown.Converter()
+
+function generateNewFeed() {
+    const rawForm = document.querySelector("#newxml")
+    const data = Object.fromEntries(new FormData(rawForm))
+    rssTitle = data.rsstitle
+    rssLink = data.rsslink
+    rssDescription = data.rssdescription
+    postTitle = data.title
+    postAuthor = data.author
+    postLink = data.link
+    isLink = postLink !== ''
+    pubDate = new Date(Date.parse(data.pubdate)).toUTCString()
+    postBody = converter.makeHtml(data.post)
+    xmlText = `<?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel><title>${rssTitle}</title>
+    <atom:link href="${rssLink}/rss.xml" rel="self" type="application/rss+xml" />
+    <link>${rssLink}</link>
+    <description>${rssDescription}</description>
+    <item>
+    <title>${postTitle}</title>
+    <pubDate>${pubDate}</pubDate>
+    <description><![CDATA[${postBody}]]></description>
+    <author>${postAuthor}</author>${isLink ? `
+        <link>${postLink}</link>` : ''}
+    <guid${isLink ? '' : ' isPermaLink="false" '}>${isLink ? postLink : crypto.randomUUID()}</guid>
+    </item>
+    </channel>
+    </rss>`
+    xmlFile = new Blob([xmlText], { type: 'text/xml' })
+    const elem = window.document.createElement('a');
+    elem.href = window.URL.createObjectURL(xmlFile);
+    elem.download = 'rss.xml';
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+}
+
+function appendPost() {
+    const rawForm = document.querySelector("#addpost")
+    const data = Object.fromEntries(new FormData(rawForm))
+    rawFeed = data.rssfeed
+    postTitle = data.title
+    postAuthor = data.author
+    postLink = data.link
+    isLink = postLink !== ''
+    pubDate = new Date(Date.parse(data.pubdate)).toUTCString()
+    postBody = converter.makeHtml(data.post)
+    let rssFeed
+    const reader = new FileReader()
+    reader.onload = (e) => {
+        rssFeed = e.target.result
+        startIndex = rssFeed.indexOf("<item>")
+        newPostText = `<item>
+        <title>${postTitle}</title>
+        <pubDate>${pubDate}</pubDate>
+        <description><![CDATA[${postBody}]]></description>
+        <author>${postAuthor}</author>${isLink ? `
+            <link>${postLink}</link>` : ''}
+        <guid${isLink ? '' : ' isPermaLink="false" '}>${isLink ? postLink : crypto.randomUUID()}</guid>
+        </item>
+        `
+        preEntry = rssFeed.slice(0, startIndex)
+        postEntry = rssFeed.slice(startIndex)
+        xmlText = preEntry + newPostText + postEntry
+        xmlFile = new Blob([xmlText], { type: 'text/xml' })
+        const elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(xmlFile);
+        elem.download = rawFeed.name;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+    }
+    reader.readAsText(rawFeed)
+}
+
+function previewText(textBox, previewBox) {
+    input = document.getElementById(textBox)
+    output = document.getElementById(previewBox)
+    output.innerHTML = converter.makeHtml(input.value)
+}
