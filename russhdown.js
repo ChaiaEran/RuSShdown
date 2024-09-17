dec026f816d3a792373c16d0f959dc962fdc13e8
Viv Lim <git@vvn.space>
2024-09-17 01:47:59 -0700

    Use XMLSerializer & DOMParser to read/write feeds
    Refactors the save & load functions to operate on xml documents in memory.
    I also am wrapping all user-input strings in CDATA sections; otherwise a user could produce a broken feed by writing a post that includes a `<`.
    
    Also with this change, upon uploading an existing feed file, the first item's author will be used to populate the author field.

--- russhdown.js
+++ russhdown.js
@@ -1,4 +1,87 @@
-var converter = new showdown.Converter()
+const converter = new showdown.Converter()
+
+var rssDoc = document.implementation.createDocument("", "", null);
+
+function attribute(aname, avalue, namespaceUri) {
+    let a;
+    if (namespaceUri === undefined){
+        a = rssDoc.createAttribute(aname);
+    }
+    else {
+        a = rssDoc.createAttributeNS(namespaceUri, aname);
+    }
+    a.value = avalue;
+    return a;
+};
+
+function appendChildElement(parent, ename, eapply, namespaceUri) {
+    let e;
+    if (namespaceUri === undefined){
+        e = rssDoc.createElement(ename);
+    }
+    else {
+        e = rssDoc.createElementNS(namespaceUri, ename);
+    }
+    if (eapply !== undefined){
+        eapply(e);
+    }
+    parent.appendChild(e);
+    return e;
+};
+
+function addCDATA(parent, string) {
+    parent.appendChild(rssDoc.createCDATASection(string));
+}
+
+function serialize(toSerialize) {
+    let serializer = new XMLSerializer();
+    let xmlString = serializer.serializeToString(toSerialize); // Doesn't indent
+
+    try {
+        console.log(`before format: ${xmlString}`);
+        let format = require('xml-formatter');
+        xmlString = format(xmlString, {
+            indentation: '  ',
+            lineSeparator: '\n',
+            throwOnFailure: false,
+
+        });
+        console.log(`after format: ${xmlString}`);
+        return xmlString;
+    }
+    catch {
+        return xmlString
+    }
+}
+
+function saveFeed(toSave, filename) {
+    let xmlText = serialize(toSave);
+    localStorage.setItem("rssfeed", xmlText)
+    localStorage.setItem("filename", filename)
+    xmlFile = new Blob([xmlText], { type: 'text/xml' })
+    const elem = window.document.createElement('a');
+    elem.href = window.URL.createObjectURL(xmlFile);
+    elem.download = filename;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+}
+
+function populateElementFromForm(item){
+    appendChildElement(item, "title", e => addCDATA(e, postTitle));
+    appendChildElement(item, "pubDate", e => addCDATA(e, pubDate));
+    appendChildElement(item, "description", e => addCDATA(e, postBody));
+    appendChildElement(item, "author", e => addCDATA(e, postAuthor));
+    if (isLink){
+        appendChildElement(item, "link", e => addCDATA(e, postLink));
+        appendChildElement(item, "guid", e => addCDATA(e, postLink));
+    } else {
+        appendChildElement(item, "guid", e => {
+            e.setAttributeNode(attribute("isPermaLink", false));
+            addCDATA(e, crypto.randomUUID());
+        });
+    }
+}
 
 function generateNewFeed() {
     const rawForm = document.querySelector("#newxml")
@@ -12,34 +95,52 @@ function generateNewFeed() {
     isLink = postLink !== ''
     pubDate = new Date(Date.parse(data.pubdate)).toUTCString()
     postBody = converter.makeHtml(data.post)
-    xmlText = `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel><title>${rssTitle}</title>
-    <atom:link href="${rssLink}/rss.xml" rel="self" type="application/rss+xml" />
-    <link>${rssLink}</link>
-    <description>${rssDescription}</description>
-    <item>
-    <title>${postTitle}</title>
-    <pubDate>${pubDate}</pubDate>
-    <description><![CDATA[${postBody}]]></description>
-    <author>${postAuthor}</author>${isLink ? `
-        <link>${postLink}</link>` : ''}
-    <guid${isLink ? '' : ' isPermaLink="false" '}>${isLink ? postLink : crypto.randomUUID()}</guid>
-    </item>
-    </channel>
-    </rss>`
-    xmlFile = new Blob([xmlText], { type: 'text/xml' })
-    localStorage.setItem("rssfeed", xmlText)
-    localStorage.setItem("filename", 'rss.xml')
-    const elem = window.document.createElement('a');
-    elem.href = window.URL.createObjectURL(xmlFile);
-    elem.download = 'rss.xml';
-    document.body.appendChild(elem);
-    elem.click();
-    document.body.removeChild(elem);
+
+    // Replace document with an empty one.
+    rssDoc = document.implementation.createDocument("", "", null);
+
+    // Scaffold the feed
+    let root = appendChildElement(rssDoc, "rss", e => {
+        e.setAttributeNode(attribute("version", "2.0"));
+        e.setAttributeNode(attribute("xmlns:atom", "http://www.w3.org/2005/Atom"));
+    });
+    let processingInstruction = rssDoc.createProcessingInstruction('xml', 'version="1.0" encoding="UTF8"');
+    rssDoc.insertBefore(processingInstruction, rssDoc.firstChild);
+    let channel = appendChildElement(root, "channel");
+
+    appendChildElement(channel, "title", e => addCDATA(e, rssTitle));
+    appendChildElement(channel, "atom:link", e => {
+        e.setAttributeNode(attribute("href", `${rssLink}/rss.xml`));
+        e.setAttributeNode(attribute("rel", "self"));
+        e.setAttributeNode(attribute("type", "application/rss+xml"));
+    });
+    appendChildElement(channel, "link", e => addCDATA(e, rssLink));
+    appendChildElement(channel, "description", e => addCDATA(e, rssDescription));
+
+    appendChildElement(channel, "item", populateElementFromForm);
+
+    saveFeed(rssDoc, 'rss.xml');
 }
 
-function appendPost() {
+async function readRssFile(f) {
+    let inputFileContents = await new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            resolve(e.target.result);
+        };
+        reader.onerror = (e) => {
+            reject(e);
+        };
+        reader.readAsText(f);
+    });
+
+    let parser = new DOMParser();
+    let rssDoc = parser.parseFromString(inputFileContents, "text/xml");
+
+    return rssDoc;
+}
+
+async function appendPost() {
     const rawForm = document.querySelector("#addpost")
     const data = Object.fromEntries(new FormData(rawForm))
     rawFeed = data.rssfeed
@@ -49,34 +150,27 @@ function appendPost() {
     isLink = postLink !== ''
     pubDate = new Date(Date.parse(data.pubdate)).toUTCString()
     postBody = converter.makeHtml(data.post)
-    let rssFeed
-    const reader = new FileReader()
-    reader.onload = (e) => {
-        rssFeed = e.target.result
-        startIndex = rssFeed.indexOf("<item>")
-        newPostText = `<item>
-        <title>${postTitle}</title>
-        <pubDate>${pubDate}</pubDate>
-        <description><![CDATA[${postBody}]]></description>
-        <author>${postAuthor}</author>${isLink ? `
-            <link>${postLink}</link>` : ''}
-        <guid${isLink ? '' : ' isPermaLink="false" '}>${isLink ? postLink : crypto.randomUUID()}</guid>
-        </item>
-        `
-        preEntry = rssFeed.slice(0, startIndex)
-        postEntry = rssFeed.slice(startIndex)
-        xmlText = preEntry + newPostText + postEntry
-        xmlFile = new Blob([xmlText], { type: 'text/xml' })
-        localStorage.setItem("rssfeed", xmlText)
-        localStorage.setItem("filename", rawFeed.name)
-        const elem = window.document.createElement('a');
-        elem.href = window.URL.createObjectURL(xmlFile);
-        elem.download = rawFeed.name;
-        document.body.appendChild(elem);
-        elem.click();
-        document.body.removeChild(elem);
+
+    // Read the rss document that was uploaded
+    let rssDoc = await readRssFile(rawFeed)
+
+    // Find the first item in the channel and add one before it.
+    let channel = rssDoc.querySelector("channel");
+    if (!channel) {
+        throw new Error("Uploaded feed didn't contain a 'channel' element.")
     }
-    reader.readAsText(rawFeed)
+    let existingFirstItem = channel.querySelector("item");
+    if (!existingFirstItem){
+        throw new Error("Uploaded feed didn't contain any items.");
+    }
+
+    let newFirstItem = rssDoc.createElement("item");
+    populateElementFromForm(newFirstItem);
+    channel.insertBefore(newFirstItem, existingFirstItem);
+
+    appendChildElement(channel, "item", populateElementFromForm);
+
+    saveFeed(rssDoc, rawFeed.name);
 }
 
 function previewText(textBox, previewBox, htmlBox) {
@@ -100,9 +194,26 @@ function checkLocalStorage(){
     }
 }
 
-function setFileName(){
-    const file = document.getElementById("rssfeedadd")
+async function setFileName(){
+    const fileElement = document.getElementById("rssfeedadd")
     const filenameSpace = document.getElementById("filename")
-    if(file.files.length !== 0)
-        filenameSpace.innerText = file.files[0].name
+    if(fileElement.files.length !== 0){
+        let file = fileElement.files[0]
+        filenameSpace.innerText = file.name
+        
+        // Attempt to read the file, get the author from the most recent item, and populate the form with it.
+        try {
+            let uploadedRssDoc = await readRssFile(file);
+            let existingFirstItem = uploadedRssDoc.querySelector("item");
+            if (existingFirstItem) {
+                let existingAuthor = existingFirstItem.querySelector("author");
+                if (existingAuthor){
+                    document.getElementById("authoradd").value = existingAuthor.textContent.trim();
+                }
+            }
+        }
+        catch (e) {
+            console.log(`Failed to set author name from uploaded feed: ${e}`)
+        }
+    }
 }
